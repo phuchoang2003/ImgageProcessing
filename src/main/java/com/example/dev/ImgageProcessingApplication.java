1a82c67193e8fb0e8145f92efd0f164fa048bdba
phuchoang2003 <phucnc2003@gmail.com>
2025-07-19 18:27:44 +0700

    Update repo

--- src/main/java/com/example/dev/ImgageProcessingApplication.java
+++ src/main/java/com/example/dev/ImgageProcessingApplication.java
@@ -1,9 +1,18 @@
 package com.example.dev;
 
+//import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+//import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "My API Documentation",
+		version = "1.0",
+		description = "This is a secured API with JWT authentication"
+))
 public class ImgageProcessingApplication {
 
 	public static void main(String[] args) {
